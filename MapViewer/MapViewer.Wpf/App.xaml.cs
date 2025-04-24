fb0cce90b6e92e10283164789075d8055533efdd
Jiří Valášek <jirka362+git@gmail.com>
2025-04-25 00:33:25 +0200

    Init project

---
+++ MapViewer/MapViewer.Wpf/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MapViewer.Core.Utils;
+using MapViewer.Core.Models;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MapViewer.Wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
+
